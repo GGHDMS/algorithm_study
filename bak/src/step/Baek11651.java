c77a938381e9691d30c1fa97eb9f78c85db0c41f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-08-18 13:44:15 +0900

    좌표 정렬하기 2

---
+++ bak/src/step/Baek11651.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek11651 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int[][] array;
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+
+        array = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[1] - o2[1];
+            }
+            return o1[1] - o2[1];
+        });
+
+        for (int[] i : array) {
+            System.out.println(i[0] + " " + i[1]);
+        }
+    }
+}
